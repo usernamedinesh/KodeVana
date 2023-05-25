5a963ebc861288705075c3416a942e7b52446267
Sahil Swargiary <sahilswargiary.personal@gmail.com>
2023-05-25 19:05:58 +0530

    Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 
-
-
-
 function App() {
   return (
     <>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,68 @@
 import React from "react";
-import "../styles/Footer.css";
+import { Link } from "react-router-dom";
+import { BsInstagram, BsFacebook, BsTwitter, BsLinkedin} from 'react-icons/bs'
+import { MdMarkEmailUnread, MdPhone, MdLocationPin} from 'react-icons/md';
+import "../styles/All.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="footer-content">
-          <div className="footer-section">
-            <h3>About Us</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum diam non venenatis molestie.</p>
-          </div>
-          <div className="footer-section">
-            <h3>Contact Us</h3>
-            <p>123 Main Street, City</p>
-            <p>Phone: (123) 456-7890</p>
-            <p>Email: info@example.com</p>
-          </div>
-          <div className="footer-section">
-            <h3>Follow Us</h3>
-            <ul className="social-links">
-              <li><a href="#"><i className="fab fa-facebook"></i></a></li>
-              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <hr />
-        <p className="footer-bottom">&copy; {new Date().getFullYear()} KodeVana. All rights reserved.</p>
+    <section id="footer">
+      <div>
+        <span id="footer-logo">
+          <h2 id="footer-logo-1">Kode</h2>
+          <h2 id="footer-logo-2">Vana</h2>
+        </span>
       </div>
-    </footer>
+
+      <div>
+        <h2>Links</h2>
+        <ul>
+          <li><Link to="/" className="links-1">Home</Link></li>
+          <li><Link to="/About" className="links-1">About Us</Link></li>
+          <li><Link to="/Services" className="links-1">Services</Link></li>
+          <li><Link to="/Projects" className="links-1">Projects</Link></li>
+          <li><Link to="/ContactUs" className="links-1">Contact Us</Link></li>
+        </ul>
+        
+      </div>
+
+      <div>
+        <h2>Services</h2>
+        <ul>
+          <li>Wev Development</li>
+          <li>App Development</li>
+          <li>UI/UX Design</li>
+          <li>Logo Design</li>
+          <li>Custom Software</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2>Contact Us</h2>
+        <ul>
+          
+          <li><MdMarkEmailUnread/>kodevana@gmail.com</li>
+          <li><MdPhone/>99999999999</li>
+          <li><MdLocationPin/>Rangia, Asssam, India</li>
+        </ul>
+      </div>
+
+      <div className="social-icon">
+        <ul>
+          <li>
+            <Link to="instagram.com"><BsInstagram color="#FFD523" size={40}/></Link>
+            <Link to="facebook.com"><BsFacebook color="#FFD523" size={40}/></Link>
+            <Link to="twitter.com"><BsTwitter color="#FFD523" size={40}/></Link>
+            <Link to="linkedin.com"><BsLinkedin color="#FFD523" size={40}/></Link>
+          </li>
+
+          <li>
+            <Link className="links-1">Privacy Policy</Link>
+            <Link className="links-1">Terms of Use</Link>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
 
